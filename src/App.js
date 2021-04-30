d612e355f0371fbd78b3931d03d9f1e7ac2194cb
Martin Van Aken <martin@joyouscoding.com>
2021-04-30 12:57:32 +0200

    First version

--- src/App.js
+++ src/App.js
@@ -2,22 +2,55 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let problems = [[
+    "Character encodings",
+    "Too many levels of indirection",
+    "Badly named variable",
+    "Timezone fun",
+    "Bad memory allocation",
+  ],[
+    "Type conversion",
+    "Race condition",
+    "Never ending loop",
+    "Rounding error",
+    "Rounding error",
+    "Off by one error",
+  ],[
+    "Rounding error",
+    "Rounding error",
+    "Joker",
+    "Rounding error",
+    "Rounding error",
+  ], [
+    "Rounding error",
+    "Rounding error",
+    "Rounding error",
+    "Rounding error",
+    "Rounding error",
+  ], [
+    "Rounding error",
+    "Rounding error",
+    "Rounding error",
+    "Rounding error",
+    "Rounding error"
+  ]
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+    <h1 className="text-4xl text-center my-10">Programmer problems bingo</h1>
+    <div className="w-full mx-5">
+      <table className="m-auto">
+        {problems.map((p) => {
+          return (
+            <tr className="">
+            {p.map((c) => {
+              return (<td className="p-2 pt-6 pb-6 align-middle text-center  border-solid border-4 border-light-blue-500">{c}</td>)
+            })}
+          </tr>
+            )
+        })}
+      </table>
+    </div>
     </div>
   );
 }
